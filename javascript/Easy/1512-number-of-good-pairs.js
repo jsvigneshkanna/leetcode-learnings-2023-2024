afb755d9fefda64a9d26d90bb3373a4df77267a6
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2023-12-28 12:05:14 +0530

    Time: 108 ms (5.92%), Space: 43.8 MB (5.12%) - LeetHub

--- javascript/Easy/1512-number-of-good-pairs.js
+++ javascript/Easy/1512-number-of-good-pairs.js
@@ -5,13 +5,24 @@
 var numIdenticalPairs = function(nums) {
     const seen = {}
     let goodPairs = 0
-    for (let index=0; index<nums.length; index++) {
-        if (nums[index] in seen) {
-            goodPairs += seen[nums[index]]
-            seen[nums[index]] += 1
+    // for (let index=0; index<nums.length; index++) {
+    //     if (nums[index] in seen) {
+    //         goodPairs += seen[nums[index]]
+    //         seen[nums[index]] += 1
+    //     }
+    //     else {
+    //         seen[nums[index]] = 1
+    //     }
+    // }
+    
+    for (num of nums) {
+        console.log(num)
+       if (num in seen) {
+            goodPairs += seen[num]
+            seen[num] += 1
         }
         else {
-            seen[nums[index]] = 1
+            seen[num] = 1
         }
     }
     return goodPairs
