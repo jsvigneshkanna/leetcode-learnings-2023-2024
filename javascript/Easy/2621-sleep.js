bc4efb60c8fbe1041c3ca0807174e3ea91ea5d86
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-01-02 19:31:44 +0530

    Time: 72 ms (5.00%), Space: 41.5 MB (65.99%) - LeetHub

---
+++ javascript/Easy/2621-sleep.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    const ms = millis * 60* 1000
+    return new Promise(function(resolve) {
+       setTimeout(resolve, millis);
+   });
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
