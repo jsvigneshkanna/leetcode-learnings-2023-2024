00a99c5cd7f8d484896b40e79d23eac1686597c7
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-01-04 00:08:54 +0530

    Time: 49 ms (69.98%), Space: 43.9 MB (5.06%) - LeetHub

--- javascript/Easy/1389-create-target-array-in-the-given-order.js
+++ javascript/Easy/1389-create-target-array-in-the-given-order.js
@@ -7,9 +7,6 @@ var createTargetArray = function(nums, index) {
     let targetArr = []
     for (let i=0; i<nums.length; i++) {
         targetArr = [...targetArr.slice(0, index[i]), ...[nums[i]], ...targetArr.slice(index[i])]
-          
-        
-        console.log(targetArr)
     }
     return targetArr
 };
