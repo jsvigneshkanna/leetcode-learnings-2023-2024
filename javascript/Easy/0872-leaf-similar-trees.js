238e3650f311083e0482067eb579a3376d0c3f78
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-01-09 20:44:21 +0530

    Time: 55 ms (75.44%), Space: 45 MB (12.38%) - LeetHub

--- javascript/Easy/0872-leaf-similar-trees.js
+++ javascript/Easy/0872-leaf-similar-trees.js
@@ -12,28 +12,22 @@
  * @return {boolean}
  */
 
-let leftLeaf = []
-let rightLeaf = []
 var leafSimilar = function(root1, root2) {
-    leftLeaf = []
-    rightLeaf = []
-    getLeaf(root1, "left")
-    getLeaf(root2, "right")
+    // get the leaves
+    const getLeaf = (root, leaves) => {
+        if (root !== null && root.left === null && root.right === null) {
+            leaves.push(root.val)
+            return
+        }
+        if (root.left) getLeaf(root.left, leaves)
+        if (root.right) getLeaf(root.right, leaves)
+    }
+    
+    const leftLeaf = []
+    const rightLeaf = []
+    getLeaf(root1, leftLeaf)
+    getLeaf(root2, rightLeaf)
     
     return leftLeaf.length == rightLeaf.length && leftLeaf.every((value, index) => value === rightLeaf[index]);
 };
 
-const getLeaf = (root, side) => {
-    if (root !== null && root.left === null && root.right === null) {
-        if (side=="left") {
-            leftLeaf.push(root.val)
-        }
-        else {
-            rightLeaf.push(root.val)
-        }
-        return
-    }
-    if (root.left) getLeaf(root.left, side)
-    if (root.right) getLeaf(root.right, side)
-    
-}
