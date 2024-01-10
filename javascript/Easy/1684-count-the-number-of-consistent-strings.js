daeba453257cd68772fce1ef8e4d0f858c0b7bf8
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-01-10 23:59:56 +0530

    Time: 100 ms (26.74%), Space: 54.9 MB (17.40%) - LeetHub

--- javascript/Easy/1684-count-the-number-of-consistent-strings.js
+++ javascript/Easy/1684-count-the-number-of-consistent-strings.js
@@ -4,18 +4,23 @@
  * @return {number}
  */
 var countConsistentStrings = function(allowed, words) {
-    allowed = new Set(allowed)
-    let consistentCount = 0
-    for (let word of words) {
-        let isConsistent = true
-        for (let char of word) {
-            if (!allowed.has(char)) {
-                isConsistent = false
-            }
-        }
-        if (isConsistent) {
-            consistentCount++
-        }
-    }
-    return consistentCount
+    // allowed = new Set(allowed)
+    // let consistentCount = 0
+    // for (let word of words) {
+    //     let isConsistent = true
+    //     for (let char of word) {
+    //         if (!allowed.has(char)) {
+    //             isConsistent = false
+    //         }
+    //     }
+    //     if (isConsistent) {
+    //         consistentCount++
+    //     }
+    // }
+    // return consistentCount
+    
+    let set = new Set(allowed)
+  return words.reduce((a, w) => {
+    return w.split('').every(l => set.has(l)) ? ++a : a
+  }, 0)
 };
