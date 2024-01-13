df219924a14178260c834701b5c4b2386ac3a8c2
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-01-13 22:39:03 +0530

    Time: 2604 ms (22.43%), Space: 0B (100.00%) - LeetHub

--- MySQL/Easy/1378-replace-employee-id-with-the-unique-identifier.sql
+++ MySQL/Easy/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
+
+# Left join 
+# takes all entries from left table and if corresponding
+# entry not there in right table, consider it as NULL
+
 select unique_id, name
 from Employees as em left join EmployeeUNI as emu
 on em.id = emu.id
