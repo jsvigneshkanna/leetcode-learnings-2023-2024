ded8d7f95222a70e14fe49eb4e0e30c1444a3559
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-01-13 18:36:03 +0530

    Time: 164 ms (25.00%), Space: 49.9 MB (13.95%) - LeetHub

--- javascript/Medium/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
+++ javascript/Medium/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -4,16 +4,20 @@
  * @return {number}
  */
 var minSteps = function(s, t) {
-    const charMap = new Array(26).fill(0)
+    // const charMap = new Array(26).fill(0)
+    const charMap = new Map()
     if (s.length !== t.length) {
         return -1
     }
     for (let index=0; index<s.length; index++) {
-        charMap[t[index].charCodeAt(0)- 'a'.charCodeAt(0)]++;
-        charMap[s[index].charCodeAt(0)- 'a'.charCodeAt(0)]--;
+        // charMap[t[index].charCodeAt(0)- 'a'.charCodeAt(0)]++;
+        // charMap[s[index].charCodeAt(0)- 'a'.charCodeAt(0)]--;
+        charMap.set(t[index], charMap.get(t[index])? charMap.get(t[index])+1: 1)
+        charMap.set(s[index], charMap.get(s[index])? charMap.get(s[index])-1: -1)
     }
     let minSteps = 0
-    for (const freq of charMap) {
+    console.log(charMap)
+    for (const freq of charMap.values()) {
         minSteps+= Math.max(0, freq)
     }
     return minSteps
