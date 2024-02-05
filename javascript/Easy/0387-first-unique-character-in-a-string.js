82b7a6dded5e55418aa8d5f9dc58f9a4a04e9368
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-02-05 21:48:09 +0530

    Time: 80 ms (80.25%), Space: 57.3 MB (6.31%) - LeetHub

---
+++ javascript/Easy/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const charCount = new Map()
+    for (const char of s) {
+        charCount[char] = (charCount[char] || 0)+ 1
+    }
+    for (let index=0; index< s.length; index++) {
+        if (charCount[s[index]] == 1) {
+            return index
+        }
+    }
+    return -1
+};
