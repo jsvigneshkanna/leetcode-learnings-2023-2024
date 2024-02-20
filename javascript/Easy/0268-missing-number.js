0cebcdf6bf0bbd94d53f877d0e14b351ed09b776
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-02-20 23:22:25 +0530

    Time: 48 ms (94.54%), Space: 51.6 MB (20.82%) - LeetHub

--- javascript/Easy/0268-missing-number.js
+++ javascript/Easy/0268-missing-number.js
@@ -3,8 +3,15 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    const numsLen = nums.length
-    const originalSum = Math.floor((numsLen* (numsLen+1))/ 2)
-    let arraySum = nums.reduce((acc, num)=> num+acc, 0)
-    return originalSum- arraySum
+    // const numsLen = nums.length
+    // const originalSum = Math.floor((numsLen* (numsLen+1))/ 2)
+    // let arraySum = nums.reduce((acc, num)=> num+acc, 0)
+    // return originalSum- arraySum
+    const n = nums.length
+    const arraySum = Math.floor((n*(n+1))/2)
+    let actualSum = 0
+    for (const num of nums) {
+        actualSum += num
+    }
+    return arraySum - actualSum
 };
