18b338fae04a0d3ebfbd0d67a4f78a80a04c377f
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-03-07 20:38:16 +0530

    Time: 44 ms (90.31%), Space: 49.3 MB (9.66%) - LeetHub

---
+++ javascript/Easy/0876-middle-of-the-linked-list.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let fast = head
+    while (fast && fast.next) {
+        head=head.next
+        fast=fast.next.next
+    }
+    return head
+};
