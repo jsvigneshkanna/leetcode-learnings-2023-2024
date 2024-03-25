f570a087e011443c29ce9c6ccb4b0dc4ba409a5c
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-03-25 19:50:39 +0530

    Time: 68 ms (97.49%), Space: 57.6 MB (80.72%) - LeetHub

--- javascript/Medium/0442-find-all-duplicates-in-an-array.js
+++ javascript/Medium/0442-find-all-duplicates-in-an-array.js
@@ -3,8 +3,23 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
-    return usingSet(nums)
+    // return usingSet(nums)
+    return usingNoSpace(nums)
 };
+
+const usingNoSpace = (nums) => {
+    const res = []
+    for (let index=0; index< nums.length; index++) {
+        if (nums[Math.abs(nums[index])-1] < 0) {
+            res.push(Math.abs(nums[index]))
+        } else {
+            nums[Math.abs(nums[index])-1] *= -1
+        }
+    }
+    
+    
+    return res
+}
 
 const usingSet = (nums) => {
     const set = new Set()
