a6cf8524017587b6dd728a2b4c57adfa012135a6
J S Vignesh Kanna <Vignesh@propertyguru.tech>
2024-03-27 22:23:31 +0530

    Time: 69 ms (64.96%), Space: 54.8 MB (32.08%) - LeetHub

---
+++ javascript/Medium/0713-subarray-product-less-than-k.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function(nums, k) {
+    if (k<=1) {
+        return 0
+    }
+    let res=0, left=0, right=0, len=nums.length, product=1
+    
+    while (right< len) {
+        product *= nums[right]
+        while (product >= k) {
+            product /= nums[left]
+            left++
+        }
+        res += right-left+1
+        right++
+    }
+    return res
+};
